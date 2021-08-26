8d49598f12d1a4a3e5ec3a19a69bcb88df22c4e7
Omar Magdy <contact.parmag@gmail.com>
2021-08-26 16:33:29 +0200

    models.py:added the Product model

--- _app/products/models.py
+++ _app/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#id,name,price,in_stock,author
+class Product(models.Model):
+	name = models.CharField(max_length=150)
+	price = models.FloatField(
+		validators=[MinValueValidator(.1),MaxValueValidator(1000*1000)])
+	in_stock =  models.BooleanField()
+
+	def __str__(self):
+		return (str(self.id) +") "+ self.name + ", "+ str(self.author))
