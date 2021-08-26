2d1c080cfe1768a9d0997fe6a1c6c22f12c240f3
Omar Magdy <contact.parmag@gmail.com>
2021-08-26 16:28:32 +0200

    celery perform update, create and delete

--- _app/app1/tasks.py
+++ _app/app1/tasks.py
@@ -7,3 +7,16 @@ from celery import shared_task
 @shared_task
 def add(x, y):
 	return x + y
+
+
+@shared_task
+def celery_perform_create():
+	pass
+
+@shared_task
+def celery_perform_update():
+	pass
+
+@shared_task
+def celery_perform_delete():
+	pass
