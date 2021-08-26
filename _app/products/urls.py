f1191b61ec8b071215db00311219e16c3513d282
Omar Magdy <contact.parmag@gmail.com>
2021-08-26 16:52:21 +0200

    urls.py:add:router

---
+++ _app/products/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from django.conf.urls import url
+from rest_framework import routers
+
+from .views import (ProductViewSet)
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register('products', ProductViewSet)
+
+
+urlpatterns = [
+    path('api/', include((router.urls,"celery_app"), namespace="api")),
+]
